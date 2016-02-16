4607596dcd976daf2a5b641273732f343c7169e0
kazukitash <kazukitash@aoikumo.jp>
2016-02-16 15:46:07 +0900

    add init file for ruby

---
+++ etc/init/osx/01_ruby.sh
@@ -0,0 +1,39 @@
+#! /bin/sh
+
+if [ -z "$DOTPATH" ]; then
+  echo '$DOTPATH not set' >&2
+  exit 1
+fi
+
+. "$DOTPATH"/etc/install
+
+cd "$DOTPATH"/etc/init/assets/ruby
+
+install_ruby() {
+  e_newline && e_header "Installing Ruby..."
+  if rbenv versions | grep -q "$RUBY_VERSION"; then
+    e_header "Ruby is already installed"
+  else
+    rbenv install $RUBY_VERSION
+    if [ $? -ne 0 ]; then
+      e_error "failed to install Ruby"
+      exit 1
+    fi
+  fi
+  rbenv global $RUBY_VERSION
+  e_done "Install"
+}
+
+install_gems() {
+  e_newline && e_header "Installing Ruby gems..."
+  rbenv exec gem install bundler
+  if [ $? -ne 0 ]; then
+    e_error "failed to install gem bundler"
+    exit 1
+  fi
+  bundle install
+  e_done "Install"
+}
+
+install_ruby
+install_gems
