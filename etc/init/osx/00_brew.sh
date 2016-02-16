4135a2134c7407d4a2f6dc4a50455ade78f16466
kazukitash <kazukitash@aoikumo.jp>
2016-02-16 15:45:58 +0900

    add init file for HomeBrew

---
+++ etc/init/osx/00_brew.sh
@@ -0,0 +1,38 @@
+#! /bin/sh
+
+if [ -z "$DOTPATH" ]; then
+  echo '$DOTPATH not set' >&2
+  exit 1
+fi
+
+. "$DOTPATH"/etc/install
+
+cd "$DOTPATH"/etc/init/assets/brew
+
+install_homebrew() {
+  e_newline && e_header "Installing HomeBrew..."
+  if has "brew"; then
+    e_header "Homebrew is already installed"
+  else
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    if [ $? -ne 0 ]; then
+      e_error "failed to install HomeBrew"
+      exit 1
+    fi
+  fi
+  e_done "Install"
+}
+
+install_formulas() {
+  e_newline && e_header "Installing HomeBrew formulas..."
+  brew tap Homebrew/bundle
+  if [ $? -ne 0 ]; then
+    e_error "failed to tap Homebrew/bundle"
+    exit 1
+  fi
+  brew bundle
+  e_done "Install"
+}
+
+install_homebrew
+install_formulas
