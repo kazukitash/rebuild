47a0cf4c0bcc88e6f7d0d0fc201a509935da371b
kazukitash <kazukitash@aoikumo.jp>
2016-02-17 14:19:31 +0900

    add git init

---
+++ etc/init/osx/04_git.sh
@@ -0,0 +1,15 @@
+if [ -z "$DOTPATH" ]; then
+  echo '$DOTPATH not set' >&2
+  exit 1
+fi
+
+. "$DOTPATH"/etc/install
+
+setup_diff_so_fancy(){
+  e_newline && e_header "Setting diff so fancy..."
+  git config --global pager.diff "diff-so-fancy | less --tabs=1,5 -RFX"
+  git config --global pager.show "diff-so-fancy | less --tabs=1,5 -RFX"
+  e_done "Set up"
+}
+
+setup_diff_so_fancy
