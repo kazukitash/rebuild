f8dba11c8bbf7f633fdff5e8471cc6b2e4e23b1f
kazukitash <kazukitash@gmail.com>
2022-10-05 07:23:39 +0900

    anyenvにlog出力を追加

--- etc/01_anyenv.sh
+++ etc/01_anyenv.sh
@@ -10,13 +10,19 @@ fi
 setup_anyenv() {
   e_newline && e_header "[Anyenv] Setting anyenv..."
   export PATH=$HOME/.anyenv/bin:$PATH
+  e_header "[Anyenv] Initializing..."
   anyenv init
   anyenv install --init
+  e_header "[Anyenv] Installing plugins..."
   mkdir -p $(anyenv root)/plugins
   git clone https://github.com/znz/anyenv-update.git $(anyenv root)/plugins/anyenv-update
+  e_header "[Anyenv] Updating..."
   anyenv update
+  e_header "[Anyenv] Installing rbenv..."
   anyenv install rbenv
+  e_header "[Anyenv] Installing nodenv..."
   anyenv install nodenv
+  e_header "[Anyenv] Installing pyenv..."
   anyenv install pyenv
   e_done "Set up"
 }
